4947647d77247cc028065be13694bf14dbc89797
Seth Veale <seth.veale@connectintouch.com>
2017-06-29 13:56:45 +1200

    Basic project setup

---
+++ Sciff.Logic/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sciff.Logic")]
+[assembly: AssemblyDescription("Core logic engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Intouch Communications Ltd")]
+[assembly: AssemblyProduct("Sciff")]
+[assembly: AssemblyCopyright("Copyright © Intouch Communications Ltd 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Sciff.Tests")]
---
+++ Sciff.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sciff.Tests")]
+[assembly: AssemblyDescription("Tests for Sciff")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Intouch Communications Ltd")]
+[assembly: AssemblyProduct("Sciff")]
+[assembly: AssemblyCopyright("Copyright © Intouch Communications Ltd 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: ComVisible(false)]
